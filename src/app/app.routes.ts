42aaf804cd14f9e6f3cd061c542a7c57966c782d
Marinus Veit <marinus.veit@isys.de>
2024-06-06 15:39:58 +0200

    add hashfunction route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'hashfunction', component: HashFunction },
+//   { path: 'about', component: AboutComponent },
+//   { path: 'contact', component: ContactComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
