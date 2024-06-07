ab913f3659f12cf157239d04f5313f5eb7a5d6a9
Marinus Veit <marinus.veit@isys.de>
2024-06-07 16:00:47 +0200

    add navbar, bootstrap, home- and signature component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HashFunctionComponent } from './hash-function/hash-function.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+//   { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'hashfunction', component: HashFunctionComponent },
   { path: 'hashfunction', component: HashFunctionComponent },
 //   { path: 'about', component: AboutComponent },
 //   { path: 'contact', component: ContactComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
