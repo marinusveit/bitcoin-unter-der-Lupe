e1b831538aa4ff96160ec70fd231721b65c69c26
Marinus Veit <marinus.veit@isys.de>
2024-06-07 18:53:04 +0200

    highlight the changing bits in hash output to show that circa half of the bits change everytime

--- src/app/hash-function/hash-function.component.ts
+++ src/app/hash-function/hash-function.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hash-function',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hash-function.component.html',
   styleUrl: './hash-function.component.scss'
 })
 export class HashFunctionComponent {
-  hash: string = '';
+  hash: string[] = [];
   hashHex: string = '';
+  previousHash: string[] = [];
 
   onInput(event: Event): void {
     const input = (event.target as HTMLInputElement).value;
-    // this.hash =  parseInt(CryptoJS.SHA256(input).toString(CryptoJS.enc.Hex), 16).toString(2);
-    this.hashHex =  CryptoJS.SHA256(input).toString(CryptoJS.enc.Hex);
-    this.hash = this.hexToBinary(this.hashHex)
-    console.log(this.hash.length)
-
+    this.hashHex = CryptoJS.SHA256(input).toString(CryptoJS.enc.Hex);
+    const newHash = this.hexToBinary(this.hashHex).split('');
+    // Update previous hash before setting the new hash
+    this.previousHash = this.hash;
+    this.hash = newHash;
   }
 
   hexToBinary(hex: string): string {
