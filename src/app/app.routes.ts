33885df61f84639448b91dc79b0cd32d664acd4e
Marinus Veit <marinus.veit@isys.de>
2024-07-03 15:33:27 +0200

    add block to navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HashFunctionComponent } from './hash-function/hash-function.component';
 import { HomeComponent } from './home/home.component';
 import { SignatureComponent } from './signature/signature.component';
+import { BlockComponent } from './block/block.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'hashfunction', component: HashFunctionComponent },
   { path: 'signatures', component: SignatureComponent },
+  { path: 'block', component: BlockComponent },
 ];
