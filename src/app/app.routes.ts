194a5c9472faf6b6aafb24d98538fb564eca0639
Marinus Veit <marinus.veit@isys.de>
2024-07-10 17:01:44 +0200

    add navbar and routes for block header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HashFunctionComponent } from './hash-function/hash-function.component';
 import { HomeComponent } from './home/home.component';
 import { SignatureComponent } from './signature/signature.component';
+import { BlockHeaderComponent } from './block-header/block-header.component';
 import { BlockComponent } from './block/block.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'hashfunction', component: HashFunctionComponent },
   { path: 'signatures', component: SignatureComponent },
+  { path: 'blockheader', component: BlockHeaderComponent },
   { path: 'block', component: BlockComponent },
 ];
