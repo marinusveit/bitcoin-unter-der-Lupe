d5235786388303e5e503d9a670db37e06a975e21
Marinus Veit <marinus.veit@isys.de>
2024-08-09 14:41:35 +0200

    after mining a block, one can add a new block wich has the hash of the previous block (wip).

--- src/app/block-chain/block-chain.component.ts
+++ src/app/block-chain/block-chain.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BlockTemplateComponent } from '../block-template/block-template.component';
 
 @Component({
   selector: 'app-block-chain',
   standalone: true,
-  imports: [BlockTemplateComponent],
+  imports: [CommonModule, BlockTemplateComponent],
   templateUrl: './block-chain.component.html',
   styleUrl: './block-chain.component.scss'
 })
 export class BlockChainComponent {
+  blocks: { previousBlockHash: string, nBits: number, transactions: any[], mined: boolean, hash: string }[] = [];
+
+  constructor() {
+    // Initialer Block ohne vorherigen Hash (Genesis Block)
+    this.blocks.push({
+      previousBlockHash: '',
+      nBits: 0x1effffff,
+      transactions: [],
+      mined: false,
+      hash: ''
+    });
+  }
+
+  // Methode, um einen neuen Block hinzuzufügen
+  addBlock(previousBlockHash: string) {
+    this.blocks.push({
+      previousBlockHash: previousBlockHash,
+      nBits: 0x1effffff,
+      transactions: [],
+      mined: false,
+      hash: ''
+    });
+  }
+
+  onBlockMined(index: number, hash: string) {
+    this.blocks[index].hash = hash
+    this.blocks[index].mined = true;
+  }
 
 }
