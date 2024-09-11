60ffa46f934eb2731f490e956af0325e22f85581
Marinus Veit <marinus.veit@isys.de>
2024-09-11 18:27:35 +0200

    component to display infos about txs

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,18 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { exampleTransactions } from '../../assets/data/transactions';
+import { TransactionTemplateComponent } from '../transaction-template/transaction-template.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TransactionTemplateComponent],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.scss'
 })
 export class TransactionComponent {
-  @Input() txId: string = '';
-  @Input() inputs: any[] = [];
-  @Input() outputs: any[] = [];
 
   transactions = exampleTransactions;
+
 }
