f87786c3c2db4a8fd00fa1c1c61bf84f00473529
Marinus Veit <marinus.veit@isys.de>
2024-09-25 13:34:21 +0200

    update transaction template so that one can switch between hex and asm to displat scriptsig and scriptpubkey

--- src/app/transaction-template/transaction-template.component.ts
+++ src/app/transaction-template/transaction-template.component.ts
@@ -6,6 +6,7 @@ import { Transaction, TxInput, TxOutput } from '../data_types/transaction';
 import { MerkleTree } from 'merkletreejs'
 import { Buffer } from 'buffer'; // wird benötigt, da ansonsten option isBitcoinTree fehler wirft
 (window as any).Buffer = Buffer;
+import * as bitcoin from 'bitcoinjs-lib';
 
 @Component({
   selector: 'app-transaction-template',
@@ -18,17 +19,75 @@ export class TransactionTemplateComponent {
   @Input() transactions: Transaction[] = [];
   @Output() txChange = new EventEmitter<Transaction[]>()
 
-  @Output() merkleRootChange = new EventEmitter<string>()  // Speichert die Merkle Root
+  @Output() merkleRootChange = new EventEmitter<string>()
   merkleRoot: string = ''
+  // Map zur Verwaltung der aktuellen Ansicht (Hex oder ASM) pro Input
+  isAsmView: { [key: string]: boolean } = {};
 
-  ngOnInit() {
+
+  hexToAsm(hex: string): string {
+    const buffer = Buffer.from(hex, 'hex');
+    var asm_array = bitcoin.script.toASM(buffer).split(' ');
+    var asm = ''
+    for (let slice of asm_array){
+      console.log(asm)
+      console.log(slice)
+      if(!slice.startsWith('OP_')){
+        asm = asm.concat(`<i class="bi bi-key-fill" title="Public Key Hash"> ${slice}</i><br>`)
+      }
+      else{
+        asm = asm.concat(slice).concat('<br>')
+      }
+    }
+    return asm
+  }
+
+  hexToAsmScriptSig(hex: string): string {
+    const buffer = Buffer.from(hex, 'hex');
+
+    // Teilen des Buffers für die Signatur und den öffentlichen Schlüssel
+    const sigLength = buffer[0]; // Erster Byte für die Signaturlänge
+    const pubKeyLength = buffer[sigLength + 1]; // Letzter Byte für die Länge des public keys
+
+    const signature = buffer.slice(1, sigLength + 1).toString('hex');
+    const publicKey = buffer.slice(sigLength + 2).toString('hex'); // 2 Bytes für die Längen
+
+    return `OP_PUSHBYTES_${sigLength}<br><i class="bi bi-vector-pen" title="Signatur"> ${signature}</i><br>OP_PUSHBYTES_${pubKeyLength}<br><i class="bi bi-key-fill" title="Public Key"> ${publicKey}</i>`;
+  }
+
+  ngAfterContentInit() {
     this.updateMerkleRoot();  // Initiale Berechnung der Merkle Root
+    this.initializeAsmView(); // Initialisiere ASM-Ansichten
+  }
+
+  // Initialisiere die ASM-Ansichten für alle Inputs und Outputs
+  initializeAsmView() {
+    for (const tx of this.transactions) {
+      for (const input of tx.inputs) {
+        // Standardmäßig Hex-Ansicht für ScriptSig
+        this.isAsmView[input.previousTxHash] = false;
+      }
+      for (const output of tx.outputs) {
+        // Standardmäßig Hex-Ansicht für ScriptPubKey
+        this.isAsmView[output.scriptPubKey] = false;
+      }
+    }
+  }
+
+  // Setzt die Ansicht auf ASM oder Hex
+  setScriptSigView(input: TxInput, isAsm: boolean) {
+    this.isAsmView[input.previousTxHash] = isAsm;
+  }
+
+  // Setzt die Ansicht auf ASM oder Hex für ScriptPubKey
+  setScriptPubKeyView(output: TxOutput, isAsm: boolean) {
+    this.isAsmView[output.scriptPubKey] = isAsm;
   }
 
   // Methode zur Berechnung der Merkle Root
   updateMerkleRoot() {
     // Extrahiere alle txIds als Blätter für den Merkle-Baum
-    const leaves = this.transactions.map(tx => crypto.SHA256(tx.txId.toString(16)))
+    const leaves = this.transactions.map(tx => crypto.SHA256(tx.txId))
     console.log(leaves.toString())
     // Erstelle den Merkle-Baum
     const tree = new MerkleTree(leaves, crypto.SHA256, { isBitcoinTree: true })
@@ -45,7 +104,7 @@ export class TransactionTemplateComponent {
     for (let tx of this.transactions) {
       if (tx.txId == transaction.txId) {
         tx = transaction
-        tx.txId = Number(crypto.SHA256(inputsString + outputsString).toString(crypto.enc.Hex));
+        tx.txId = crypto.SHA256(inputsString + outputsString).toString(crypto.enc.Hex);
         this.updateMerkleRoot();  // Initiale Berechnung der Merkle Root
         this.txChange.emit(this.transactions)  // Emitiere die neue Transaktions-ID
         this.merkleRootChange.emit(this.merkleRoot)
